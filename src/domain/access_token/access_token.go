94e0d397cd44c83ced13f735e486b4e8d286d2a4
ian <muhia>
2021-09-08 18:13:28 +0300

    get user by id from db

--- src/domain/access_token/access_token.go
+++ src/domain/access_token/access_token.go
@@ -2,25 +2,24 @@ package access_token
 
 import "time"
 
-const(
+const (
 	expirationTime = 24
 )
 
-type AccessToken struct{
+type AccessToken struct {
 	AccessToken string `json:"access_token"`
-	UserId int64  `json:"user_id"`
-	ClientId int64 `json:"client_id"`
-	Expires int64 `json:"expires"`
-
+	UserId      int64  `json:"user_id"`
+	ClientId    int64  `json:"client_id"`
+	Expires     int64  `json:"expires"`
 }
 
 func GetNewAccessToken() AccessToken {
 	return AccessToken{
-		Expires: time.Now().UTC().Add(expirationTime * time.Hour ).Unix(),
+		Expires: time.Now().UTC().Add(expirationTime * time.Hour).Unix(),
 	}
 }
 
-func (at AccessToken) IsExpired()bool{
+func (at AccessToken) IsExpired() bool {
 	//now := t ime.Now().UTC()
 	//expirationTime := time.Unix(at.Expires, 0)
 
