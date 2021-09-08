e0b3e8a3883fa5452c4fbd34a090a36ea1db474c
ian <muhia>
2021-09-08 13:35:41 +0300

    repository interface and service interfaces

--- src/domain/access_token/service.go
+++ src/domain/access_token/service.go
@@ -1,4 +1,27 @@
 package access_token
 
+import "bookstore_oauth-api/src/domain/utils/errors"
 
-type
+type Repository interface{
+	GetById(string)(*AccessToken, *errors.RestErr)
+
+}
+
+
+type Service interface {
+	GetById(string)(*AccessToken, *errors.RestErr)
+}
+
+type service struct {
+	repository Repository
+}
+
+func NewService(repo Repository) Service{
+	return &service{
+		repository: repo,
+	}
+}
+
+func (s *service)GetById(string)(*AccessToken, *errors.RestErr){
+	return nil, nil
+}
