288bbfba62bb06270983743969374deff338e86b
ian <ianmuhia3@gmail.com>
2021-09-17 18:39:28 +0300

    added resty package for restclient

--- src/repository/rest/users_repository.go
+++ src/repository/rest/users_repository.go
@@ -1,23 +1,23 @@
 package rest
 
 import (
+	"encoding/json"
+	"fmt"
+
+	"github.com/go-resty/resty/v2"
+
 	"bookstore_oauth-api/src/domain/users"
 	"bookstore_oauth-api/src/domain/utils/errors"
-	"encoding/json"
-
-	//"os/user"
-	"time"
-
-	"github.com/mercadolibre/golang-restclient/rest"
 )
 
 var (
-	usersRestClient = rest.RequestBuilder{
-		Headers:        nil,
-		Timeout:        100 * time.Millisecond,
-		ConnectTimeout: 0,
-		BaseURL:        "https://api.bookstore.com",
-	}
+	client = resty.New()
+	// usersRestClient = rest.RequestBuilder{
+	// 	Headers:        nil,
+	// 	Timeout:        100 * time.Millisecond,
+	// 	ConnectTimeout: 0,
+	// 	BaseURL:        "https://api.bookstore.com",
+	// }
 )
 
 type RestUserRepository interface {
@@ -31,25 +31,38 @@ func NewRepository() RestUserRepository {
 }
 
 func (s *usersRepository) LoginUser(email, password string) (*users.User, *errors.RestErr) {
+	// client := resty.New()
 	request := users.UserLoginRequest{
 		Email:    email,
 		Password: password,
 	}
-	response := usersRestClient.Post("/users/login", request)
-	if response == nil || response.Response == nil {
-		return nil, errors.NewInternalServerError("invalid response when trying to login user")
-	}
-	if response.StatusCode > 299 {
-		var restErr errors.RestErr
-		err := json.Unmarshal(response.Bytes(), &restErr)
-		if err != nil {
-			return nil, errors.NewInternalServerError("invalid error interface when trying to login user")
 
-		}
-		return nil, &restErr
+	response, err := client.R().
+		SetBody(request).
+		//   SetResult(&AuthSuccess{}).    // or SetResult(AuthSuccess{}).
+		//   SetError(&AuthError{}).       // or SetError(AuthError{}).
+		Post("/users/login")
+	if err != nil {
+		panic(err)
+
 	}
+
+	fmt.Println(response.Body())
+	// response := usersRestClient.Post("/users/login", request)
+	// if response == nil || response.Response == nil {
+	// 	return nil, errors.NewInternalServerError("invalid response when trying to login user")
+	// }
+	// if response.StatusCode > 299 {
+	// 	var restErr errors.RestErr
+	// 	err := json.Unmarshal(response.Bytes(), &restErr)
+	// 	if err != nil {
+	// 		return nil, errors.NewInternalServerError("invalid error interface when trying to login user")
+
+	// 	}
+	// 	return nil, &restErr
+	// }
 	var user users.User
-	if err := json.Unmarshal(response.Bytes(), &user); err != nil {
+	if err := json.Unmarshal(response.Body(), &user); err != nil {
 		return nil, errors.NewInternalServerError("error when trying to unmarshal users response")
 	}
 	return &user, nil
--- src/repository/rest/users_repository_test.go
+++ src/repository/rest/users_repository_test.go
@@ -1,37 +1,69 @@
 package rest
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"testing"
+// import (
+// 	"fmt"
+// 	"log"
+// 	"os"
+// 	"testing"
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+// 
 
-	"github.com/mercadolibre/golang-restclient/rest"
-)
+// 	"github.com/mercadolibre/golang-restclient/rest"
+// )
 
-func TestMain(m *testing.M) {
-	log.Println("about to start mock up server...")
-	rest.StartMockupServer()
-	// rest.Mock
-	os.Exit(m.Run())
-}
+// func TestMain(m *testing.M) {
+// 	log.Println("about to start mock up server...")
+// 	rest.StartMockupServer()
+// 	// rest.Mock
+// 	os.Exit(m.Run())
+// }
 
-func TestLoginUserTimeoutFromApi(t *testing.T) {
-	repository := usersRepository{}
-	user, err := repository.LoginUser("email@gmail.com", "password")
+// func TestLoginUserTimeoutFromApi(t *testing.T) {
+// 	repository := usersRepository{}
+// 	user, err := repository.LoginUser("email@gmail.com", "password")
 
-	fmt.Println(user)
-	fmt.Println(err)
-}
-func TestLoginUserInvalidErrorInterface(t *testing.T) {
+// 	fmt.Println(user)
+// 	fmt.Println(err)
+// }
+// func TestLoginUserInvalidErrorInterface(t *testing.T) {
 
-}
-func TestLoginUserInvalidUserCredentials(t *testing.T) {
+// }
+// func TestLoginUserInvalidUserCredentials(t *testing.T) {
 
-}
-func TestLoginUserJsonResponse(t *testing.T) {
+// }
+// func TestLoginUserJsonResponse(t *testing.T) {
 
-}
-func TestLoginUserNoError(t *testing.T) {
+// }
+// func TestLoginUserNoError(t *testing.T) {
 
-}
+// }
